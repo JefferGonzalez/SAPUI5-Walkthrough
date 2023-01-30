d3c4814c2ec76522eef73c07c8b966a3dccf9ffc
JefferGonzalez <jefferd2016@gmail.com>
2023-01-30 10:06:37 -0500

    Feat: Finish Step 32: Routing Back and History

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,22 +1,36 @@
 // eslint-disable-next-line no-undef
-sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
-  'use strict'
-  return Controller.extend('sap.ui.webapp.controller.Detail', {
-    onInit: function () {
-      const oRouter = this.getOwnerComponent().getRouter()
-      oRouter
-        .getRoute('detail')
-        .attachPatternMatched(this._onObjectMatched, this)
-    },
-    _onObjectMatched: function (oEvent) {
-      this.getView().bindElement({
-        path:
-          '/' +
-          window.decodeURIComponent(
-            oEvent.getParameter('arguments').invoicePath
-          ),
-        model: 'invoice'
-      })
-    }
-  })
-})
+sap.ui.define(
+  ['sap/ui/core/mvc/Controller', 'sap/ui/core/routing/History'],
+  function (Controller, History) {
+    'use strict'
+    return Controller.extend('sap.ui.webapp.controller.Detail', {
+      onInit: function () {
+        const oRouter = this.getOwnerComponent().getRouter()
+        oRouter
+          .getRoute('detail')
+          .attachPatternMatched(this._onObjectMatched, this)
+      },
+      _onObjectMatched: function (oEvent) {
+        this.getView().bindElement({
+          path:
+            '/' +
+            window.decodeURIComponent(
+              oEvent.getParameter('arguments').invoicePath
+            ),
+          model: 'invoice'
+        })
+      },
+      onNavBack: function () {
+        const oHistory = History.getInstance()
+        const sPreviousHash = oHistory.getPreviousHash()
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1)
+        } else {
+          const oRouter = this.getOwnerComponent().getRouter()
+          oRouter.navTo('overview', {}, true)
+        }
+      }
+    })
+  }
+)
